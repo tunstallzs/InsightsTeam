34f7b9f7e39fcc51979b2b5c3dc4339d513939ca
RashedRifat <59259308+RashedRifat@users.noreply.github.com>
2021-02-08 13:58:17 -0500

    helloWorld tutorial

---
+++ tutorial0/hello.py
@@ -0,0 +1,15 @@
+# Welcome To Lecture0!
+# By this time, you should have already installed Python, VS Code and Git/Github Desktop!
+# We will being writing Python in this file shortly! Stay tuned! 
+# You may have noticed #'s in front of the first few lines - these are intentinal.
+# Whatever we write in this file will be interpreted by Python when we execute 
+# this file. As such, if we want to write in English (in full sentences), 
+# we use #'s. This is tells Python to treat this line as a comment, which is useful for 
+# human eyes. Python will ignore these # lines and will not execute them - perfect for wiritng notes! 
+
+
+# Lets say hello to the world! 
+
+print("Hello World!")
+your_name = input("What is your name? ")
+print(f"\nHello, {your_name}! Welcome to coding in Python!\n")
